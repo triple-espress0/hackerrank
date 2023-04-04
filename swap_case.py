67781c4c77fed2f293207ab428b7ec662b163fb9
Angel Todorov <angel.k.todorov@gmail.com>
2023-04-04 10:56:00 +0300

    -

---
+++ swap_case.py
@@ -0,0 +1,10 @@
+'''hackerrank "sWAP cASE" challenge '''
+
+def swap_case(s):
+    return s.swapcase()
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
+    
