88bdba478793137355d8aae152e20cda8e264a1a
Angel Todorov <angel.k.todorov@gmail.com>
2023-07-13 13:07:05 +0300

    c

---
+++ map_lambda.py
@@ -0,0 +1,14 @@
+"""hackerrank 'Map and Lambda Function' challenge """
+cube = lambda x: x**3
+
+
+def fibonacci(n):
+    fibs = [0, 1]
+    for i in range(2, n):
+        fibs.append(fibs[-1] + fibs[-2])
+    return fibs
+
+
+if __name__ == "__main__":
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
